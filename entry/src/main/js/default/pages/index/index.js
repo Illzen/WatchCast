dec0a64053f4f24caeddea532ee0547735890a83
Illzen <908413385@qq.com>
2023-05-30 00:53:20 +0800

    loath

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,8 +1,40 @@
+import router from '@system.router';
+import fetch from 'node-fetch';
+
 export default {
-    data: {
-        title: ""
-    },
     onInit() {
-        this.title = this.$t('strings.world');
+
+        let KEY = "8289fc0cb3cc706f3078a8c86bea911f";
+        let ID = "1810821";
+        const UNIT = 'metric';
+
+        fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${KEY}&id=${
+        ID}&units=${UNIT}`)
+            .then(response => response.json())
+            .then(data => {
+                // Do something with the data
+                console.log(data);
+            })
+            .catch(error => {
+                // Handle any errors
+                console.error(error);
+            });
     }
 }
+
+//export default {
+//    data: {
+//        title: ""
+//    },
+//    onInit() {
+//        console.log("index.js/onInit() is evoked");
+//    },
+//    //        JSON.parse(https://api.openweathermap.org/data/2.5/forecast?id=1810821&appid=8289fc0cb3cc706f3078a8c86bea911f);
+//    toWeatherPage(e) {
+//        if (e.direction == 'down') {
+//            router.replace({
+//                uri: 'pages/weather/weather'
+//            });
+//        }
+//    }
+//}
--- entry/src/main/js/default/pages/weather/weather.js
+++ entry/src/main/js/default/pages/weather/weather.js
@@ -1,21 +1,68 @@
-const fs = require('fs');
+//export default {
+//    data: {
+//        title: ""
+//    },
+//    onInit() {
+//        this.title = "Hello World";
+//    }
+//}
+//const request = require('request');
+//var fs = require('fs');
+//
+//
+//
+//let rawdata = fs.readFileSync('credentials.json');
+//let config = JSON.parse(rawdata);
+//
+//let API_KEY = config.KEY;
+//let ID = config.ID;
+//const UNIT = 'metric';
+//
+//fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${API_KEY}&id=${
+//ID}&units=${UNIT}`)
+//    .then(response => response.json())
+//    .then(data => {
+//        // Do something with the data
+//        console.log(data);
+//    })
+//    .catch(error => {
+//        // Handle any errors
+//        console.error(error);
+//    });
 
-let rawdata = fs.readFileSync('credentials.json');
-let config = JSON.parse(rawdata);
 
-let KEY = config.KEY;
-let ID = config.ID;
 
-const UNIT = 'metric';
+const forecast = function (latitude, longitude) {
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${KEY}&id=${
-          ID}&units=${UNIT}`)
-    .then(response => response.json())
-    .then(data => {
-      // Do something with the data
-      console.log(data);
+    var url = `http://api.openweathermap.org/data/2.5/weather?`
+    +`lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+
+    request({ url: url, json: true }, function (error, response) {
+        if (error) {
+            console.log('Unable to connect to Forecast API');
+        }
+        else {
+
+            console.log('It is currently '
+            + response.body.main.temp
+            + ' degrees out.'
+            );
+
+            console.log('The high today is '
+            + response.body.main.temp_max
+            + ' with a low of '
+            + response.body.main.temp_min
+            );
+
+            console.log('Humidity today is '
+            + response.body.main.humidity
+            );
+        }
     })
-    .catch(error => {
-      // Handle any errors
-      console.error(error);
-    });
+}
+
+var latitude = 22.7196; // Indore latitude
+var longitude = 75.8577; // Indore longitude
+
+// Function call
+forecast(latitude, longitude);
