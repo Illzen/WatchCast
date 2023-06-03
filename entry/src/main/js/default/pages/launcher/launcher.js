230beedcb2a95a61f93a01ee463d94e4df5478d0
Illzen <908413385@qq.com>
2023-06-04 01:12:26 +0800

    Watch swipe down Launcher

--- entry/src/main/js/default/pages/launcher/launcher.js
+++ entry/src/main/js/default/pages/launcher/launcher.js
@@ -2,7 +2,20 @@ import router from '@system.router';
 
 export default {
     data: {
-        title: ""
+        title: "",
+        logoList:[ {
+            src : "/pages/image/logo3.png",
+            text : "Calculator"
+        },{
+            src : "/pages/image/logo1.png",
+            text : "Radio"
+        },{
+            src : "/pages/image/logo2.png",
+            text : "Terminal"
+        },{
+            src : "/pages/image/logo.png",
+            text : "WatchCast"
+        }]
     },
     onInit() {
         this.title = "Hello World";
@@ -13,7 +26,14 @@ export default {
                 uri: 'pages/index/index'
             });
         }
+    },
+    toWeatherPage(){
+        router.replace({
+            uri: 'pages/weather/weather'
+        });
     }
 }
+
+
 
 
